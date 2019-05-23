1f639dd24fad67eaf84e5bba52f0c0e29c49ff08
Andrew O <andrew@codesquad.org>
2019-05-22 20:48:07 -0400

    added app.js and images

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express'),
+					app = express();
+
+// app.set('view engine', 'ejs');
+app.use('/img', express.static('img'));
+
+app.get('/', (req, res) => res.render('reservation.ejs'));
+app.get('/orders', (req, res) => res.render('orders/new.ejs'));
+
+// CREATE NEW ORDER IN DATABASE
+app.post('/order', (req, res) => {
+  // Save user input from request body into individual variables
+  var client = req.body.client;
+  var bags = req.body.bags;
+  // Create a new order and save to database
+  Order.create({
+    client: client,
+    bags: bags
+  }, function (err, newOrder){
+    if(err){
+      console.log(err);
+    }
+  })
+  // Redirect back to order form
+  res.redirect('/');
+});
+
+app.listen(3000, () => console.log('Fair Foods Reservation System listening on port 3000!'))
